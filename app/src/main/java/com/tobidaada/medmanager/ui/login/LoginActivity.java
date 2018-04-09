97820a87b759c16af76ec4051afcacf0286dd3f5
Tobi Daada <daadatobi@gmail.com>
2018-04-09 09:08:26 +0100

    Implement onClickListener for login button

--- app/src/main/java/com/tobidaada/medmanager/ui/login/LoginActivity.java
+++ app/src/main/java/com/tobidaada/medmanager/ui/login/LoginActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.tobidaada.medmanager.R;
+import com.tobidaada.medmanager.ui.medication.MedicationActivity;
 import com.tobidaada.medmanager.ui.sign_up.SignUpActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,6 +16,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button loginButton = findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMedicationActivity();
+            }
+        });
     }
 
 
@@ -23,4 +33,12 @@ public class LoginActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    public void openMedicationActivity() {
+        Intent intent = new Intent(LoginActivity.this, MedicationActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+
 }
